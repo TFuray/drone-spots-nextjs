5501a9d546324d22f8dfdf27d986de33ed552dc6
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-09-02 23:26:25 -0700

    added find my location functionality and made location search more robust

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,12 @@
 'use client'
+import { Button } from '@rewind-ui/core'
 import axios from 'axios'
 import { ChangeEvent, useState } from 'react'
 import { useCoordinatesStore, useWeatherStore } from 'store/store'
 import { Coordinates, WeatherData } from 'types/weather'
-import { Button } from '@rewind-ui/core'
 
 const SearchBar = () => {
-  const [location, setLocation] = useState('95403')
+  const [location, setLocation] = useState('')
   const setWeatherData = useWeatherStore(state => state.setWeatherData)
   const setCoordinates = useCoordinatesStore(state => state.setCoordinates)
   const latitude = useCoordinatesStore(state => state.latitude)
@@ -16,10 +16,10 @@ const SearchBar = () => {
     setLocation(e.target.value)
   }
 
-  const getWeatherData = async () => {
+  const getWeatherData = async (input) => {
     try {
       const response = await axios.get<WeatherData>(
-        `https://api.weatherapi.com/v1/current.json?key=2b97f312100a4bd099b180935230707 &q=${location}`
+        `https://api.weatherapi.com/v1/current.json?key=2b97f312100a4bd099b180935230707 &q=${input}`
       )
       setWeatherData(response.data)
       setCoordinates({
@@ -32,8 +32,31 @@ const SearchBar = () => {
   }
 
   const handleClick = () => {
-    getWeatherData()
+    getWeatherData(location)
   }
+
+  const handleFindMyLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error)
+    } else {
+      console.log('Geolocation not supported')
+    }
+    function success(position: GeolocationPosition) {
+      const userLat: number = position.coords.latitude
+      const userLong: number = position.coords.longitude
+      console.log(userLat,userLong)
+      setCoordinates({
+        latitude: userLat,
+        longitude: userLong
+      })
+      setLocation(`${userLat}, ${userLong}`)
+
+      getWeatherData(`${userLat},${userLong}`)
+    }
+      function error(error: GeolocationPositionError) {
+        console.log(`Unable to retrieve your location ${error}`)
+      }
+    }
 
   return (
     <div className='grid grid-cols-3 gap-2 search-bar '>
@@ -44,10 +67,13 @@ const SearchBar = () => {
         value={location}
         onChange={handleInputChange}
       />
-      <div className='grid'>
+      <div className='flex gap-1'>
         {/* <Button color='black' size='sm'>Search</Button> */}
         <button className='btn grid btn-outline border-secondary border-2' onClick={handleClick}>
           Search
+        </button>
+        <button className='btn grid btn-outline border-secondary border-2' onClick={handleFindMyLocation}>
+         Find My <br/>Location
         </button>
       </div>
     </div>
