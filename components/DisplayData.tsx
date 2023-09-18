028dccba08c6cc17adcf7934b2a7a0274cedc1e6
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-09-17 22:49:08 -0700

    wkorking on displaying results

--- components/DisplayData.tsx
+++ components/DisplayData.tsx
@@ -15,43 +15,6 @@ import Spot from './Spot'
 const DisplayData = (): JSX.Element => {
 
   const data = useWeatherStore(state => state.weatherData)
-  console.log(data)
-  const displayDirection = () => {
-    if (!data) return <div>loading</div>
-    if (data.current.wind_dir === 'N') {
-      return <North />
-    } else if (data.current.wind_dir === 'NNE') {
-      return <NorthEast />
-    } else if (data.current.wind_dir === 'NE') {
-      return <East />
-    } else if (data.current.wind_dir === 'ENE') {
-      return <SouthEast />
-    } else if (data.current.wind_dir === 'E') {
-      return <South />
-    } else if (data.current.wind_dir === 'ESE') {
-      return <SouthEast />
-    } else if (data.current.wind_dir === 'SE') {
-      return <SouthEast />
-    } else if (data.current.wind_dir === 'SSE') {
-      return <SouthEast />
-    } else if (data.current.wind_dir === 'S') {
-      return <South />
-    } else if (data.current.wind_dir === 'SSW') {
-      return <SouthWest />
-    } else if (data.current.wind_dir === 'SW') {
-      return <SouthWest />
-    } else if (data.current.wind_dir === 'WSW') {
-      return <SouthWest />
-    } else if (data.current.wind_dir === 'W') {
-      return <West />
-    } else if (data.current.wind_dir === 'WNW') {
-      return <NorthWest />
-    } else if (data.current.wind_dir === 'NW') {
-      return <NorthWest />
-    } else if (data.current.wind_dir === 'NNW') {
-      return <NorthWest />
-    }
-  }
 
   return (
     <>
@@ -73,7 +36,6 @@ const DisplayData = (): JSX.Element => {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </>
   )
 }
